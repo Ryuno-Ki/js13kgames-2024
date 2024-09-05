ad704726ae8bb406b88f12212782a3431203ccde
André Jaenisch <andre.jaenisch@posteo.de>
2024-09-05 15:39:59 +0200

    refactor: rename pickup clause to offer
    This is a better name for its semantics.
    
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- types/state/clauses/offer.d.ts
+++ types/state/clauses/offer.d.ts
@@ -1,9 +1,9 @@
 /**
- * Clause to check available items for pickup.
+ * Clause to check available items for offer.
  *
  * @argument {import('../initial-state.js').State} state
  * @returns {Array<import('../initial-state.js').ValueFlowsEconomicResource>}
  */
-export function pickup(
+export function offer(
   state: import("../initial-state.js").State,
 ): Array<import("../initial-state.js").ValueFlowsEconomicResource>;
